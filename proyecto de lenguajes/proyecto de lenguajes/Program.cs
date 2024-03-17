d5170a7b5d9ba796cbf1a917b5c86a31a57f3e73
xTony0x <jabelteton@correo.utl.edu.gt>
2024-03-17 17:27:01 -0600

    Creacion de proyecto

---
+++ proyecto de lenguajes/proyecto de lenguajes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_de_lenguajes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
