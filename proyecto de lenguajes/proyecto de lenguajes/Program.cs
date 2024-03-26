050fbcb6be42002b9b13838d067388a620748952
xXTony04Xx <149640212+xXTony04Xx@users.noreply.github.com>
2024-03-25 18:22:25 -0600

    menu de lecutra de archivos y lectura de forma manual, creacion de objeto y prueba de guardar datos en tabla hash

--- proyecto de lenguajes/proyecto de lenguajes/Program.cs
+++ proyecto de lenguajes/proyecto de lenguajes/Program.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Linq.Expressions;
 
 
 namespace Proyecto_Lenguajes
 {
+    class Automata
+    {
+        public string inicial;
+        public int noDeEstados;
+        public string inicio;
+        public string transicion;
+        public string final;
+
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -21,37 +31,114 @@ namespace Proyecto_Lenguajes
             Console.WriteLine("3. salir");
             Console.WriteLine();
             opcion = int.Parse(Console.ReadLine());
+            Console.Clear();
 
             switch (opcion)
             {
                 case 1://determinista
 
-                    LeerTxt();
+                    LeerCarpeta(@"C:\Users\antho\OneDrive\Escritorio\Automatas deterministas");
                     break;
                 case 2://no determinista
-                    LeerTxt();
+                   
                     break;
                 case 3://salir
 
 
 
-                    Console.WriteLine("Hola");
+                    Console.WriteLine("Adios");
+                    Console.ReadKey();
                     break;
 
             }
         }
 
-        static void LeerTxt()
+        static void LeerCarpeta(string carpeta)
         {
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\antho\OneDrive\Escritorio\Automatas deterministas\automata fin 11.txt");
-            System.Console.WriteLine("Contenido del archivo = ");
-            foreach (string line in lines)
+            int contadorTemp = 1;
+            int opcion2 = 0;
+            string[] archivos = Directory.GetFiles(carpeta);
+
+            Console.WriteLine("Archivos en la carpeta:");
+
+            foreach (string archivo in archivos)
             {
-                // Use a tab to indent each line of the file.
-                Console.WriteLine("\t" + line);
+                Console.WriteLine( contadorTemp + ". " + Path.GetFileName(archivo));
+                contadorTemp++;
             }
-            Console.WriteLine("Presione cualquier tecla para salir.");
-            System.Console.ReadKey();
+
+            Console.WriteLine("Seleccione el archivo a abrir y si desea enviar una ruta de forma manual, seleccione el no.6");
+            opcion2 = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            switch(opcion2)
+                {
+                case 1:
+                    LeerArchivo(@"C:\Users\antho\OneDrive\Escritorio\Automatas deterministas\(0,1) 0 multiplos de 3 y 1 multiplos de 4 (1).txt");
+
+                    break;
+
+                case 2:
+                    LeerArchivo(@"C:\Users\antho\OneDrive\Escritorio\Automatas deterministas\automata fin 11.txt");
+                    break;
+
+                case 3:
+                    LeerArchivo(@"C:\Users\antho\OneDrive\Escritorio\Automatas deterministas\autómata no.2 – (abc  (ab))  (cc).txt");
+                    break;
+
+                case 4:
+                    LeerArchivo(@"C:\Users\antho\OneDrive\Escritorio\Automatas deterministas\automata3.txt");
+                    break;
+
+                case 5:
+                    LeerArchivo(@"C:\Users\antho\OneDrive\Escritorio\Automatas deterministas\automata4.txt");
+                    break;
+
+                case 6:
+                    string rutaArchivo = null;
+                    Console.WriteLine("ha escogido la opcion de ingreso manual, pegue la ruta de su archivo .txt");
+                    rutaArchivo = Console.ReadLine();
+
+                    if (!string.IsNullOrEmpty(rutaArchivo))
+                    {
+                        // Verificar si el archivo existe en la ruta especificada
+                        if (File.Exists(rutaArchivo))
+                        {
+                            // Llamar a la función para leer el archivo
+                            LeerArchivo(rutaArchivo);
+                        }
+                        else
+                        {
+                            Console.WriteLine("El archivo no existe en la ruta especificada.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("La ruta ingresada es nula o vacía. No se puede continuar.");
+                    }
+
+                    break;
+            }
+            
+           
+        }
+
+        static void LeerArchivo(string rutaArchivo)
+        {
+
+            try
+            {
+                
+                string contenido = File.ReadAllText(rutaArchivo);
+                Console.WriteLine("Contenido del archivo:");
+                Console.WriteLine(contenido);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Se produjo un error al intentar leer el archivo:");
+                Console.WriteLine(ex.Message);
+            }
+
         }
     }
 }
